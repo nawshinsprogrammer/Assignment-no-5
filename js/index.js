6006d1f129f58ac4b052c5a87ba4939d18036ebd
nawshin <nawshinsiddiqa@gmail.com>
2024-09-24 13:55:56 +0600

    added main ,footer

--- js/index.js
+++ js/index.js
@@ -9,12 +9,26 @@ document.getElementById('donate-amount')
       newBalance=navAmount-inputAmount
      
      document.getElementById('nav-amount').innerText=newBalance
-     alert('successfully!Done')
+   
     }
     
     else{
-      alert('your account does not have enough balance')
+      alert('It is not valid')
+      
+
     }
+
+    document.getElementById('my_modal_5').showModal()
+
+    const historyItem =document.createElement("div");
+      historyItem.className = "bg-white p-3 rounded-md border";
+      historyItem.innerHTML=`
+      <p>inputAmount:${inputAmount}</p>
+      <p></p>
+      <p class="text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
+      `
+    const historyContainer =document.getElementById('history-section');
+    historyContainer.appendChild(historyItem)
     
 })
 
@@ -30,16 +44,35 @@ document.getElementById('donate-amount1')
       newBalance=navAmount-inputAmount
      
      document.getElementById('nav-amount').innerText=newBalance
-     alert('successfully!Done')
+     
     }
     
     else{
-      alert('your account does not have enough balance')
+      alert('It is not valid')
     }
+
+
+    document.getElementById('my_modal_5').showModal()
+
+    const historyItem =document.createElement("div");
+      historyItem.className = "bg-white p-3 rounded-md border";
+      historyItem.innerHTML=`
+      <p>inputAmount:${inputAmount}</p>
+      <p></p>
+      <p class="text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
+      `
+    const historyContainer =document.getElementById('history-section');
+    historyContainer.appendChild(historyItem)
+    
+
+    console.log(historyItem)
+    console.log(historyContainer)
     
 })
 document.getElementById('donate-amount2')
+
 .addEventListener('click',function(){
+  console.log('text')
     const inputAmount =parseFloat(document.getElementById('input-amount2').value)
     const bdtAmount =parseFloat(document.getElementById('bdt-amount2').innerText)
       document.getElementById('bdt-amount2').innerText=inputAmount
@@ -49,39 +82,60 @@ document.getElementById('donate-amount2')
       newBalance=navAmount-inputAmount
      
      document.getElementById('nav-amount').innerText=newBalance
-     alert('successfully!Done')
+  
     }
     
     else{
-      alert('your account does not have enough balance')
+     alert('It is not valid')
     }
+    document.getElementById('my_modal_5').showModal()
+
 
     const historyItem =document.createElement("div");
       historyItem.className = "bg-white p-3 rounded-md border";
-      historyItem.innerHtml=`
-      <p>inputAmount:$${inputAmount}</p>
-      <p class="text-xs text-gray-500">${newDate().toLocalDateString()}</p>
+      historyItem.innerHTML=`
+      <p>inputAmount:${inputAmount}</p>
+      <p></p>
+      <p class="text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
       `
     const historyContainer =document.getElementById('history-section');
-    historyContainer.insertBefore(historyItem,historyContainer.firstChild);
+    historyContainer.appendChild(historyItem)
+    
+
+    console.log(historyItem)
+    console.log(historyContainer)
+    // historyContainer.insertBefore(historyItem,historyContainer.firstChild);
 })
+
+
+
 
   // history tab
   const historyTab =document.getElementById('history-tab')
   const donationTab =document.getElementById('donation-tab')
  
   historyTab.addEventListener('click',function(){
+
+    
+
+
+
+
     historyTab.classList.add('text-2xl','font-bold','bg-lime-400')
     donationTab.classList.remove('text-2xl','font-bold','bg-lime-400')
     document.getElementById('main-id').classList.add('hidden')
-    document.getElementById('history-section').classList.remove('hidden')
-
+   
+   document.getElementById('history-section').classList.remove('hidden')
+   
     
   })
 
   document.getElementById('donation-tab').addEventListener('click',function(){
    
-    donationTab.classList.add('main-id');
+    document.getElementById('main-id').classList.remove('hidden');
+    historyTab.classList.remove('text-2xl','font-bold','bg-lime-400')
+    donationTab.classList.add('text-2xl','font-bold','bg-lime-400')
+    document.getElementById('history-section').classList.add('hidden')
   })
  
   document.getElementById('blog-id').addEventListener('click',function(){
